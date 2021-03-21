f7868e7eab18661f81c30470f5c7c9cd76f5f257
Damian Lis <foxprogramming93@gmail.com>
2021-03-21 23:34:38 +0100

    Modified fluentBlog.js and main.js added handlers fn

--- scripts/sites/fluentBlog.js
+++ scripts/sites/fluentBlog.js
@@ -1,5 +1,6 @@
-import '../theme.js'
+import { themeHandler } from '../handlers/index.js'
 import { DataArrange } from '../objects/index.js'
 import data from '../../data/sets/projects/fluentBlog.js'
 
+themeHandler()
 new DataArrange('#container', data)
--- scripts/sites/main.js
+++ scripts/sites/main.js
@@ -1,7 +1,9 @@
-import '../post.js'
-import '../theme.js'
+import posts from '../../data/sets/posts.js'
+import { postHandler, themeHandler } from '../handlers/index.js'
 import Sound from '../objects/Sound.js'
 import Form from '../objects/Form.js'
 
+themeHandler()
+postHandler(posts)
 new Sound()
 new Form()
