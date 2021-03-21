d46fd6679ce2459b7bd83ebdedbecdefb440d0c5
Damian Lis <foxprogramming93@gmail.com>
2021-03-21 15:44:06 +0100

    Expanded range of addPropsAfterDelay fn by adding classes and style elements

--- scripts/helpers/addPropsAfterDelay.js
+++ scripts/helpers/addPropsAfterDelay.js
@@ -1,17 +1,28 @@
-export default ({ node, properties, delay }) => {
-  let el = node
-  if (typeof node === 'string') {
-    el = document.querySelector(node)
-  }
-  if (delay) {
-    setTimeout(() => {
-      for (let property in properties) {
-        el.style[property] = properties[property]
-      }
-    }, delay)
-  } else {
-    for (let property in properties) {
-      el.style[property] = properties[property]
+export default (data, delay) => {
+  data.map((dataEl) => {
+    const { node, classes, styleElements, properties } = dataEl
+    let el = node
+    if (typeof node === 'string') {
+      el = document.querySelector(node)
     }
-  }
+    if (delay) {
+      setTimeout(() => {
+        for (let styleElement in styleElements) {
+          el.style[styleElement] = styleElements[styleElement]
+        }
+        for (let property in properties) {
+          el[property] = properties[property]
+        }
+
+        classes && el.classList.add(...classes)
+      }, delay)
+    } else {
+      for (let styleElement in styleElements) {
+        el.style[styleElement] = styleElements[styleElement]
+      }
+      for (let property in properties) {
+        el[property] = properties[property]
+      }
+    }
+  })
 }
