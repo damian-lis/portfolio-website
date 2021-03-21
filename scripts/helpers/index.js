d809db8729a7ca9039f82235088cf8978c825711
Damian Lis <foxprogramming93@gmail.com>
2021-03-21 16:12:06 +0100

    Added createElementFn to index.js to export

--- scripts/helpers/index.js
+++ scripts/helpers/index.js
@@ -3,6 +3,7 @@ import removeAllActive from './removeAllActive.js'
 import setActive from './setActive.js'
 import setVariables from './setVariables.js'
 import addPropsAfterDelay from './addPropsAfterDelay.js'
+import createElementFn from './createElementFn.js'
 
 export {
   initialSetActive,
@@ -10,4 +11,5 @@ export {
   setActive,
   setVariables,
   addPropsAfterDelay,
+  createElementFn,
 }
