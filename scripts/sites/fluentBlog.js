12495efafbd6e1491060e1de7fd355d5ad53d60f
Damian Lis <foxprogramming93@gmail.com>
2021-03-23 18:56:59 +0100

    Added Form object to fluentBlog.js

--- scripts/sites/fluentBlog.js
+++ scripts/sites/fluentBlog.js
@@ -1,6 +1,7 @@
 import themes from '../../data/global/themes.js'
 import data from '../../data/sets/projects/fluentBlog.js'
-import { DataArrange, Theme, Particles } from '../objects/index.js'
+import { DataArrange, Theme, Particles, Form } from '../objects/index.js'
 
 new Theme('#theme-settings', themes, Particles)
+new Form('#btn-left-container')
 new DataArrange('#container', data)
