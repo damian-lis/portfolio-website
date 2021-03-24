64e84780a5c3783eeef19a7ae6aff95805b0b4d3
Damian Lis <foxprogramming93@gmail.com>
2021-03-24 09:33:17 +0100

    Changed containers sent in sites/main.js and sites/fluentBlog.js

--- scripts/sites/fluentBlog.js
+++ scripts/sites/fluentBlog.js
@@ -3,5 +3,5 @@ import data from '../../data/sets/projects/fluentBlog.js'
 import { DataArrange, Theme, Particles, Form } from '../objects/index.js'
 
 new Theme('#theme-settings', themes, Particles)
-new Form('#btn-left-container')
+new Form('.global-left-container')
 new DataArrange('#container', data)
--- scripts/sites/main.js
+++ scripts/sites/main.js
@@ -2,7 +2,7 @@ import posts from '../../data/sets/posts.js'
 import themes from '../../data/global/themes.js'
 import { Sound, Form, Posts, Theme, Particles } from '../objects/index.js'
 
-new Sound('#btn-top-container')
+new Sound('.global-left-container')
+new Form('.global-left-container')
 new Theme('#theme-settings', themes, Particles)
-new Form('#btn-left-container')
 new Posts('.post-wrapper', '#post-section', posts)
