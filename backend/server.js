871f9be8637f3dfd8ddb48facc320e24b587d6df
Damian Lis <foxprogramming93@gmail.com>
2021-03-25 14:59:44 +0100

    Added called cors fn and sendMail controller on /api/mail route in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,22 @@
 import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import { sendMail } from './mail.js'
+import cors from 'cors'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
-app.use('/api/mail', async (req, res) => {
-  console.log(req.body)
-  res.json({ message: 'Email sent!' })
-})
+app.use('/api/mail', sendMail)
 
 const PORT = 5000
 
