f0d90929295f0c756f274f3067b5e92343c2fecc
Damian Lis <foxprogramming93@gmail.com>
2021-03-25 14:54:29 +0100

    Created mail.js with nodemailer and mailgun settings for handling emails

---
+++ backend/mail.js
@@ -0,0 +1,31 @@
+import nodemailer from 'nodemailer'
+import mailGun from 'nodemailer-mailgun-transport'
+import { MAILGUN_API_KEY, MAILGUN_DOMAIN } from './env.js'
+
+const auth = {
+  auth: {
+    api_key: MAILGUN_API_KEY,
+    domain: MAILGUN_DOMAIN,
+  },
+}
+
+const transporter = nodemailer.createTransport(mailGun(auth))
+
+export const sendMail = (req, res) => {
+  const { email, subject, name, message } = req.body
+
+  const mailOptions = {
+    from: email,
+    to: 'damian.lis1293@gmail.com',
+    subject: `Portfolio - ${subject}`,
+    html: `<p><strong>From: </strong>${name}</p><p>${message}</p>`,
+  }
+
+  transporter.sendMail(mailOptions, (err) => {
+    if (err) {
+      res.status(500).json({ message: 'Internal error' })
+    } else {
+      res.json({ message: 'Email has been sent!' })
+    }
+  })
+}
