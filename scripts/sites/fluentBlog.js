e6653b55b71cd9703f9ebf61939d185eaf4b1b0a
Damian Lis <foxprogramming93@gmail.com>
2021-03-29 19:00:26 +0200

    Improved html structure and readability by better selected class names and id

--- scripts/sites/fluentBlog.js
+++ scripts/sites/fluentBlog.js
@@ -2,6 +2,6 @@ import themes from '../../data/global/themes.js'
 import data from '../../data/sets/projects/fluentBlog.js'
 import { DataArrange, Theme, Particles, Form } from '../objects/index.js'
 
-new Theme('#theme-settings', themes, Particles)
-new Form('.global-left-container')
-new DataArrange('#container', data)
+new Theme('#theme', themes, Particles)
+new Form('#global-left-container')
+new DataArrange('#project-description', data)
--- scripts/sites/main.js
+++ scripts/sites/main.js
@@ -2,7 +2,7 @@ import posts from '../../data/sets/posts.js'
 import themes from '../../data/global/themes.js'
 import { Sound, Form, Posts, Theme, Particles } from '../objects/index.js'
 
-new Sound('.global-left-container', '#post-section')
-new Form('.global-left-container', '#post-section')
-new Theme('#theme-settings', themes, Particles)
-new Posts('.post-wrapper', '#post-section', posts)
+new Sound('#global-left-container', '#posts-trigger')
+new Form('#global-left-container', '#posts-trigger')
+new Theme('#theme', themes, Particles)
+new Posts('#posts', '#posts-trigger', '#posts-wrapper', posts)
