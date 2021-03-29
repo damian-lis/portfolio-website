ac13122fb644bfd8ab19271c17ccaa276d2c25e4
Damian Lis <foxprogramming93@gmail.com>
2021-03-29 18:48:08 +0200

    Added ability to add styles and remove if statement on text prop in createElementFn.js

--- scripts/helpers/createElementFn.js
+++ scripts/helpers/createElementFn.js
@@ -3,9 +3,6 @@ export default ({ element, cb, ...rest }) => {
 
   if (Object.keys(rest).length) {
     for (const propEl in rest) {
-      if (propEl === 'text') {
-        createdElement.textContent = rest[propEl]
-      }
       if (propEl === 'event') {
         createdElement.addEventListener(rest[propEl], (e) => {
           cb ? cb(e) : () => {}
@@ -13,6 +10,11 @@ export default ({ element, cb, ...rest }) => {
       }
       if (propEl === 'classes') {
         createdElement.classList.add(...rest[propEl])
+      }
+      if (propEl === 'styles') {
+        rest[propEl].map((styleObj) => {
+          createdElement.style[styleObj.name] = styleObj.value
+        })
       }
       createdElement[propEl] = rest[propEl]
     }
