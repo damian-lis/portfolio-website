ebc75d9f8f3bfb69df65902a1687327f047ac981
Damian Lis <foxprogramming93@gmail.com>
2021-04-09 12:22:09 +0200

    Expanded setClassesFn helper with type check

--- scripts/helpers/setClassesFn.js
+++ scripts/helpers/setClassesFn.js
@@ -1,3 +1,9 @@
-export default (element, classesToAdd) => {
-  element.classList.add(...classesToAdd)
+export default (objects) => {
+  objects.map((object) => {
+    let element = object.element
+    if (typeof object.element === 'string') {
+      element = document.querySelector(object.element)
+    }
+    element.classList.add(...object.classes)
+  })
 }
