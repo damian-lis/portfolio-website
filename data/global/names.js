9e92d1e5a1b55fc8468f7ee47982a52163b3a0b0
Damian Lis <foxprogramming93@gmail.com>
2021-04-09 11:12:36 +0200

    Created data/global/names.js where saved most data used in the project

---
+++ data/global/names.js
@@ -0,0 +1,155 @@
+export const idNames = {
+  global: {
+    mainContainer: 'global-main-container',
+    leftContainer: 'global-left-container',
+  },
+
+  posts: {
+    hook: 'posts-hook',
+    wrapper: 'posts-wrapper',
+  },
+
+  preview: {
+    cornerTl: 'preview-corner-tl',
+    cornerTr: 'preview-corner-tr',
+    cornerBr: 'preview-corner-br',
+    cornerBl: 'preview-corner-bl',
+  },
+
+  theme: {
+    canvas: 'theme-canvas',
+  },
+}
+
+export const classNames = {
+  curtain: {
+    container: 'curtain-container',
+    active: 'curtain-active',
+  },
+  form: {
+    card: 'form-card',
+    titleContainer: 'form-title-container',
+    title: 'form-title',
+    outerContainer: 'form-outer-container',
+    innerContainer: 'form-inner-container',
+    main: 'form',
+    field: 'form-field',
+    fieldMessage: 'form-field-message',
+    btnDeleteContainer: 'form-btn-delete-container',
+    btnDelete: 'form-btn-delete',
+    spinnerContainer: 'form-spinner-container',
+    spinner: 'form-spinner',
+  },
+  global: {
+    leftBtn: 'global-left-btn',
+  },
+
+  posts: {
+    hook: 'posts-hook',
+    wrapper: 'posts-wrapper',
+    container: 'posts-container',
+  },
+
+  post: {
+    linkWrapper: 'post-link-wrapper',
+    container: 'post-container',
+    thubnail: 'post-thubnail',
+    preview: 'post-preview',
+    title: 'post-title',
+    intro: 'post-intro',
+    iconsContainer: 'post-icons-container',
+    ribbon: 'post-ribbon',
+    ribbonText: 'post-ribbon-text',
+  },
+
+  preview: {
+    container: 'preview-container',
+    shadow: 'preview-shadow',
+    corner: 'preview-corner',
+  },
+
+  skills: {
+    content: 'skills-content',
+    headline: 'skills-headline',
+    contentHeader: 'skills-content-header',
+    contentListItem: 'skills-content-list-item',
+  },
+
+  theme: {
+    container: 'theme-container',
+    optionsContainer: 'theme-options-container',
+    optionsDot: 'theme-options-dot',
+    note: 'theme-note',
+    optionsDotActive: 'theme-options-dot-active',
+  },
+
+  animations: {
+    slideInFromLeft: ' slideInFromLeft',
+    slideInFromRight: 'slideInFromRight',
+    slideInFromTop: 'slideInFromTop',
+    showObject: 'showObject',
+  },
+
+  utilities: {
+    animations: {
+      slideInFromTop: 'slideInFromTop',
+    },
+    text: {
+      center: 'text-center',
+      justify: 'text-justify',
+      lh25: 'text-lh-25',
+      smLeft: 'sm-text-left',
+    },
+    margin: {
+      t5: 'mt-5',
+      t10: 'mt-10',
+      t40: 'mt-40',
+      b20: 'mb-20',
+      y20: 'my-20',
+      y10: 'my-10',
+    },
+    border: {
+      rounded: 'rounded',
+      danger: 'border-danger',
+    },
+
+    width: {
+      full: 'w-full',
+    },
+
+    height: {
+      full: 'h-full',
+    },
+  },
+}
+
+export const idReferences = {
+  global: {
+    mainContainer: '#global-main-container',
+    leftContainer: '#global-left-container',
+  },
+  posts: {
+    main: '#posts',
+    trigger: '#posts-trigger',
+    wrapper: '#posts-wrapper',
+  },
+  theme: {
+    main: '#theme',
+  },
+  project: {
+    description: '#project-description',
+  },
+}
+
+export const src = {
+  sound: {
+    playImg: '../../data/images/icons/stopMusic.svg',
+    pauseImg: '../../data/images/icons/playMusic.svg',
+    audioRecord: '../data/musics/ambient.mp3',
+  },
+  form: {
+    btnImg: '../../data/images/icons/email.svg',
+  },
+}
+
+export const mailEndPoint = 'http://localhost:5000/api/mail'
