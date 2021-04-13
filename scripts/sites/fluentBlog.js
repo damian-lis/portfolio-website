adbb97f604fa633f3a42a3a5bf102a47da19e951
Damian Lis <foxprogramming93@gmail.com>
2021-04-13 19:49:12 +0200

    Fixed path names to files in fluentBlog.js

--- scripts/sites/fluentBlog.js
+++ scripts/sites/fluentBlog.js
@@ -1,7 +1,7 @@
-import themes from '../../data/global/themes.js'
-import data from '../../data/sets/projects/fluentBlog.js'
+import themes from '/data/global/themes.js'
+import data from '/data/sets/projects/fluentBlog.js'
 import { DataArrange, Theme, Particles, Form } from '../objects/index.js'
-import { idReferences } from '../../data/global/names.js'
+import { idReferences } from '/data/global/names.js'
 
 new Theme(idReferences.theme.main, themes, Particles)
 new Form(idReferences.global.leftContainer)
