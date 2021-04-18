4de1778556ddc0c8b1c5b8b61ae6863cc35ca194
Damian Lis <foxprogramming93@gmail.com>
2021-04-18 20:45:59 +0200

    Added BackBtn and Sound objects to fluentBlog.js and privatePolicy.js

--- scripts/sites/fluentBlog.js
+++ scripts/sites/fluentBlog.js
@@ -1,8 +1,17 @@
 import themes from '/data/global/themes.js'
 import data from '/data/sets/projects/fluentBlog.js'
-import { DataArrange, Theme, Particles, Form } from '../objects/index.js'
+import {
+  DataArrange,
+  Theme,
+  Particles,
+  Form,
+  BackBtn,
+  Sound,
+} from '../objects/index.js'
 import { idReferences } from '/data/global/names.js'
 
 new Theme(idReferences.theme.main, themes, Particles)
-new Form(idReferences.global.leftContainer)
 new DataArrange(idReferences.project.description, data)
+new BackBtn(idReferences.global.leftContainer)
+new Sound(idReferences.global.leftContainer, idReferences.posts.trigger)
+new Form(idReferences.global.leftContainer)
