9eb28518df17a4b139c4cdb70eb2c3600d671ea5
Damian Lis <foxprogramming93@gmail.com>
2021-05-05 18:59:15 +0200

    Deleted addPropsAfterDelayFn helper

--- scripts/helpers/index.js
+++ scripts/helpers/index.js
@@ -1,7 +1,6 @@
 import removeClassesFn from './removeClassesFn.js'
 import setClassesFn from './setClassesFn.js'
 import setVariablesFn from './setVariablesFn.js'
-import addPropsAfterDelayFn from './addPropsAfterDelayFn.js'
 import createElementFn from './createElementFn.js'
 import triggerActionOnWindowScrollFn from './triggerActionOnWindowScrollFn.js'
 import appendElementsToContainerFn from './appendElementsToContainerFn.js'
@@ -13,7 +12,6 @@ export {
   removeClassesFn,
   setClassesFn,
   setVariablesFn,
-  addPropsAfterDelayFn,
   createElementFn,
   triggerActionOnWindowScrollFn,
   appendElementsToContainerFn,
