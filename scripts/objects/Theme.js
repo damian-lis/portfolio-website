d06af951ab95b10306a4247d204ecf0970c82afb
Damian Lis <foxprogramming93@gmail.com>
2021-05-05 18:57:56 +0200

    Refactored objects/Theme.js

--- scripts/objects/Theme.js
+++ scripts/objects/Theme.js
@@ -1,127 +1,119 @@
 import {
   createElementFn,
-  setClassesFn,
-  removeClassesFn,
   appendElementsToContainerFn,
+  setActiveFn,
 } from '../helpers/index.js'
 import { classNames } from '../../data/global/names.js'
 
 class Theme {
   constructor(container, themesObject, BackgroundObj) {
     const containerSent = document.querySelector(container)
-    const initialThemeName = this.getThemeNameFromLocalStorage(themesObject)
-    const initialThemeObject = this.setThemeObject(
-      themesObject,
-      initialThemeName
-    )
-    const themeElements = this.createThemeElements(
-      themesObject,
-      initialThemeName
-    )
-    const themeComponent = this.joinThemeElements(themeElements)
+    this.themesObject = themesObject
+    this.BackgroundObj = BackgroundObj
+    this.initialThemeName = this.setInitialThemeName()
+    this.initialThemeObject = this.setInitialThemeObject()
 
-    appendElementsToContainerFn([themeComponent], containerSent)
-    this.setGlobalVariables(initialThemeObject)
-    this.createBackgroundAnimation(BackgroundObj, initialThemeObject)
+    this.createElements()
+    this.createComponents()
+    this.setGlobalVariables()
+    this.createBackgroundAnimation()
+
+    appendElementsToContainerFn([this.themeComponent], containerSent)
+  }
+
+  createElements() {
+    this.background = new this.BackgroundObj()
+
+    this.themeContainer = createElementFn({
+      element: 'div',
+      classes: [classNames.theme.container],
+    })
+
+    this.themeTitle = createElementFn({
+      element: 'h5',
+      textContent: 'Personalize Theme',
+    })
+
+    this.themeOptionsContainer = createElementFn({
+      element: 'div',
+      classes: [classNames.theme.optionsContainer],
+    })
+
+    this.themeOptionsDots = Object.keys(this.themesObject).map((themeName) => {
+      return createElementFn({
+        element: 'div',
+        classes: [
+          classNames.theme.optionsDot,
+          themeName === this.initialThemeName &&
+            classNames.theme.optionsDotActive,
+        ],
+        id: themeName,
+        listeners: [
+          {
+            event: 'click',
+            cb: (e) => this.handleDotClick({ element: e.target, themeName }),
+          },
+        ],
+      })
+    })
+
+    this.themeNote = createElementFn({
+      element: 'p',
+      classes: [classNames.theme.note],
+      textContent: '*Theme settings will be saved for your next visit',
+    })
+  }
+
+  createComponents() {
+    this.themeOptionsComponent = appendElementsToContainerFn(
+      this.themeOptionsDots,
+      this.themeOptionsContainer
+    )
+
+    this.themeComponent = appendElementsToContainerFn(
+      [this.themeTitle, this.themeOptionsComponent, this.themeNote],
+      this.themeContainer
+    )
+  }
+
+  handleDotClick({ element, themeName }) {
+    const themeObject = this.themesObject[themeName]
+
+    setActiveFn({
+      setOn: element,
+      removeFrom: this.themeOptionsDots,
+      classes: [classNames.theme.optionsDotActive],
+    })
+    this.setGlobalVariables(themeObject)
+    this.background.setTheme(themeObject)
+    this.saveThemeNameInLocalStorage(themeName)
   }
 
   setGlobalVariables(themeObject) {
-    for (const property in themeObject) {
+    for (const property in themeObject
+      ? themeObject
+      : this.initialThemeObject) {
       document.documentElement.style.setProperty(
         `--${property}`,
-        themeObject[property]
+        themeObject ? themeObject[property] : this.initialThemeObject[property]
       )
     }
   }
 
-  setThemeObject(themesObject, themeName) {
-    return themesObject[themeName]
+  setInitialThemeName() {
+    return localStorage.getItem('theme') || Object.keys(this.themesObject)[0]
   }
 
-  getThemeNameFromLocalStorage(themesObject) {
-    return localStorage.getItem('theme') || Object.keys(themesObject)[0]
+  setInitialThemeObject() {
+    return this.themesObject[this.initialThemeName]
   }
 
   saveThemeNameInLocalStorage(theme) {
     localStorage.setItem('theme', theme)
   }
 
-  createBackgroundAnimation(BackgroundObj, themeObject) {
-    this.background = new BackgroundObj()
-    this.background.start(themeObject)
-  }
-
-  createThemeElements(themesObject, themeName) {
-    this.themeContainer = createElementFn({
-      element: 'div',
-      classes: [classNames.theme.container],
-    })
-    this.themeTitle = createElementFn({
-      element: 'h5',
-      textContent: 'Personalize Theme',
-    })
-    this.themeOptionsContainer = createElementFn({
-      element: 'div',
-      classes: [classNames.theme.optionsContainer],
-    })
-    this.themeOptionsDots = Object.keys(themesObject).map((theme) => {
-      return createElementFn({
-        element: 'div',
-        classes: [
-          classNames.theme.optionsDot,
-          theme === themeName && classNames.theme.optionsDotActive,
-        ],
-        id: theme,
-        listeners: [
-          {
-            event: 'click',
-            cb: (e) => {
-              removeClassesFn(this.themeOptionsDots, [
-                classNames.theme.optionsDotActive,
-              ])
-              setClassesFn([
-                {
-                  element: e.target,
-                  classes: [classNames.theme.optionsDotActive],
-                },
-              ])
-              this.setGlobalVariables(themesObject[theme])
-              this.background.setTheme(themesObject[theme])
-              this.saveThemeNameInLocalStorage(theme)
-            },
-          },
-        ],
-      })
-    })
-    this.themeNote = createElementFn({
-      element: 'p',
-      classes: [classNames.theme.note],
-      textContent: '*Theme settings will be saved for your next visit',
-    })
-
-    return [
-      this.themeContainer,
-      this.themeTitle,
-      this.themeOptionsContainer,
-      this.themeOptionsDots,
-      this.themeNote,
-    ]
-  }
-
-  joinThemeElements(elements) {
-    const [
-      themeContainer,
-      themeTitle,
-      themeOptionsContainer,
-      themeOptionsDots,
-      themeNote,
-    ] = elements
-
-    themeOptionsDots.map((dot) => themeOptionsContainer.appendChild(dot))
-    const elementsToAppend = [themeTitle, themeOptionsContainer, themeNote]
-    elementsToAppend.map((element) => themeContainer.appendChild(element))
-
-    return this.themeContainer
+  createBackgroundAnimation() {
+    this.background.start(this.initialThemeObject)
   }
 }
 
