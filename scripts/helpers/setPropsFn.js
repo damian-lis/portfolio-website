f1729f24aaada3f23f96efea1fe2e303f32bc636
Damian Lis <foxprogramming93@gmail.com>
2021-05-06 15:18:22 +0200

    Removed brackets from functions that do not require them in setPropsFn helper

--- scripts/helpers/setPropsFn.js
+++ scripts/helpers/setPropsFn.js
@@ -1,34 +1,29 @@
 export default (objs, delay) => {
   if (delay) {
-    return setTimeout(() => {
-      console.log(delay)
-      objs.map((obj) => {
-        obj.elements.map((element) => {
-          if (obj.styleProps) {
-            obj.styleProps.map((prop) => {
-              element.style[prop.name] = prop.value
-            })
-          } else if (obj.props) {
-            obj.props.map((prop) => {
-              element[prop.name] = prop.value
-            })
-          }
-        })
-      })
-    }, delay)
+    return setTimeout(
+      () =>
+        objs.map((obj) =>
+          obj.elements.map((element) => {
+            if (obj.styleProps) {
+              obj.styleProps.map(
+                (prop) => (element.style[prop.name] = prop.value)
+              )
+            } else if (obj.props) {
+              obj.props.map((prop) => (element[prop.name] = prop.value))
+            }
+          })
+        ),
+      delay
+    )
   } else {
-    objs.map((obj) => {
+    objs.map((obj) =>
       obj.elements.map((element) => {
         if (obj.styleProps) {
-          obj.styleProps.map((prop) => {
-            element.style[prop.name] = prop.value
-          })
+          obj.styleProps.map((prop) => (element.style[prop.name] = prop.value))
         } else if (obj.props) {
-          obj.props.map((prop) => {
-            element[prop.name] = prop.value
-          })
+          obj.props.map((prop) => (element[prop.name] = prop.value))
         }
       })
-    })
+    )
   }
 }
