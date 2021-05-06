5bd8ff45e6e611c1609631dd65adc1ba80efb5b1
Damian Lis <foxprogramming93@gmail.com>
2021-05-06 20:54:10 +0200

    Refactored scripts/objects/DataArrange.js file

--- scripts/objects/DataArrange.js
+++ scripts/objects/DataArrange.js
@@ -2,108 +2,104 @@ import {
   createElementFn,
   appendElementsToContainerFn,
 } from '../helpers/index.js'
-import { classNames, common } from '../../data/global/names.js'
+import { common } from '../../data/global/names.js'
 
 class DataArrange {
   constructor(container, data) {
-    const containerSent = document.querySelector(container)
+    this.containerSent = document.querySelector(container)
     this.data = data
 
     this.createElements()
-    appendElementsToContainerFn(this.elements, containerSent)
+    appendElementsToContainerFn(this.elements, this.containerSent)
   }
 
   createElements() {
-    this.elements = this.data.map((element) => {
-      const elName = Object.keys(element)[0]
-      switch (elName) {
+    this.elements = this.data.map((object) => {
+      switch (object.type) {
         case common.headline:
           const headline = createElementFn({
-            element: common.elements.h(2),
-            classes: [
-              classNames.utilities.text.center,
-              classNames.utilities.margin('t', 10),
-            ],
-            textContent: element.headline,
+            element: object.element,
+            classes: object.classes,
+            textContent: object.content,
           })
 
           return headline
 
-        case common.title:
+        case common.header:
           const title = createElementFn({
-            element: common.elements.h(3),
-            textContent: element.title,
-            classes: [
-              classNames.utilities.margin('t', 40),
-              classNames.utilities.margin('b', 20),
-            ],
+            element: object.element,
+            textContent: object.content,
+            classes: object.classes,
           })
 
           return title
 
         case common.image:
           const image = createElementFn({
-            element: common.elements.img,
-            classes: [
-              classNames.utilities.border.rounded,
-              classNames.utilities.width.full,
-              classNames.utilities.margin('y', 20),
-            ],
-            src: element.image,
+            element: object.element,
+            classes: object.classes,
+            src: object.content,
           })
 
           return image
 
-        case common.text:
-          return element.text.map((el) => {
-            const text = createElementFn({
-              element: common.elements.p,
-              classes: [
-                classNames.utilities.margin('y', 10),
-                classNames.utilities.text.justify,
-                classNames.utilities.text.smLeft,
-                classNames.utilities.text.lh(25),
-              ],
-              textContent: el,
+        case common.paragraph:
+          return object.content.map((paragraphContent) => {
+            const paragraph = createElementFn({
+              element: object.element,
+              classes: object.classes,
+              textContent: paragraphContent,
             })
 
-            return text
+            return paragraph
           })
 
-        case common.links:
-          return element.links.map((linkEl) => {
-            const link = createElementFn({
-              element: common.elements.a,
-              target: common.props.values.blank,
-              classes: [
-                classNames.utilities.text.justify,
-                classNames.utilities.text.smLeft,
-              ],
-              href: linkEl.path,
-              textContent: linkEl.linkText,
+        case common.link:
+          const link = createElementFn({
+            element: object.elements.link,
+            target: common.props.values.sblank,
+            classes: object.classes.link,
+            href: object.path,
+            textContent: object.content.link,
+          })
+          const paragraph = createElementFn({
+            element: object.elements.paragraph,
+            classes: object.classes.paragraph,
+            textContent: object.content.paragraph,
+          })
+          paragraph.appendChild(link)
+
+          return paragraph
+
+        case common.list:
+          const list = createElementFn({
+            element: object.elements.list,
+          })
+
+          const listItems = object.content.map((listItemContent) =>
+            createElementFn({
+              element: object.elements.listItem,
+              classes: object.classes.listItem,
+              textContent: listItemContent,
             })
-            const text = createElementFn({
-              element: common.elements.p,
-              classes: [classNames.utilities.margin('y', 10)],
-              textContent: linkEl.label,
-            })
-            text.appendChild(link)
+          )
 
-            return text
-          })
+          listItems.map((listItem) => list.appendChild(listItem))
 
-        case common.code:
-          const pre = createElementFn({
-            element: common.elements.pre,
-          })
-          const code = createElementFn({
-            element: common.elements.code,
-            classes: [common.classNames.prism.languageJS],
-            textContent: element.code,
-          })
-          pre.appendChild(code)
+          return list
 
-          return pre
+        // case common.code:
+        //   const pre = createElementFn({
+        //     element: common.elements.pre,
+        //   })
+        //   const code = createElementFn({
+        //     element: common.elements.code,
+        //     classes: [common.classNames.prism.languageJS],
+        //     textContent: object.value,
+        //   })
+        //   pre.appendChild(code)
+
+        //   return pre
 
         default:
           break
