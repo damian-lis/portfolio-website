4675c574b9899ee8fab1f83c81b3cbbeb3b0a89d
Damian Lis <foxprogramming93@gmail.com>
2021-05-06 21:02:19 +0200

    Moved the way of creating about and skills description from html file to main.js via DataArrange.js

--- scripts/sites/main.js
+++ scripts/sites/main.js
@@ -1,8 +1,19 @@
 import posts from '/data/sets/posts.js'
 import themes from '/data/global/themes.js'
-import { Sound, Form, Posts, Theme, Particles } from '../objects/index.js'
+import {
+  Sound,
+  Form,
+  Posts,
+  Theme,
+  Particles,
+  DataArrange,
+} from '../objects/index.js'
 import { idReferences } from '/data/global/names.js'
+import skillsData from '/data/sets/skills.js'
+import aboutData from '/data/sets/about.js'
 
+new DataArrange(idReferences.about.description, aboutData)
+new DataArrange(idReferences.skills.description, skillsData)
 new Sound(idReferences.global.leftContainer, idReferences.posts.trigger)
 new Form(idReferences.global.leftContainer, idReferences.posts.trigger)
 new Theme(idReferences.theme.main, themes, Particles)
