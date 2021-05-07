cea294d08c6244a578bf8bd6cfcd17f11f79acb9
Damian Lis <foxprogramming93@gmail.com>
2021-05-07 10:10:34 +0200

    Created export local files from data/descriptions/projects and data/descriptions

---
+++ data/descriptions/index.js
@@ -0,0 +1,12 @@
+import about from './about.js'
+import privatePolicy from './privatePolicy.js'
+import skills from './skills.js'
+import { fluentBlog, sneakPeeks } from './projects/index.js'
+
+export {
+  about as aboutDescription,
+  privatePolicy as privatePolicyDescription,
+  skills as skillsDescription,
+  fluentBlog as fluentBlogDescription,
+  sneakPeeks,
+}
---
+++ data/descriptions/projects/index.js
@@ -0,0 +1,4 @@
+import fluentBlog from './fluentBlog.js'
+import sneakPeeks from './sneakPeeks.js'
+
+export { fluentBlog, sneakPeeks }
