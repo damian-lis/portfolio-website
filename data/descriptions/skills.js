f268defb920a5a9ddc0c1f1d7df8bccd8b48a237
Damian Lis <foxprogramming93@gmail.com>
2021-05-16 20:43:44 +0200

    Changed texts and the form of variables in skills.js

--- data/descriptions/skills.js
+++ data/descriptions/skills.js
@@ -4,18 +4,18 @@ export default [
   {
     type: common.headline,
     element: elements.h(3),
-    content: 'My skills',
+    content: 'Skills',
     classes: [
-      classNames.utilities.margin('t', 10),
+      classNames.utilities.m.t(10),
       classNames.utilities.text.center,
+      classNames.utilities.text.underline,
     ],
   },
-
   {
     type: common.header,
     element: elements.h(5),
-    content: '1. Level enough to write more advanced frontend',
-    classes: [classNames.utilities.margin('t', 30), ,],
+    content: '1. Frontend:',
+    classes: [classNames.utilities.m.t(30), classNames.utilities.text.lh(30)],
   },
   {
     type: common.list,
@@ -24,23 +24,36 @@ export default [
       listItem: elements.li,
     },
     content: [
-      '- HTML (Semanthic writing),',
-      '- CSS (SASS, BEM, TailwindCSS and basic level of Bootstrap),',
-      '- JavaScript (OOP, Design Patterns, Functional Programming),',
-      '- React (Redux, Hooks, Styled Components, Compound Components etc.),',
-      '- Next (basic understanding the concept of server side rendering),',
+      'HTML (semantic),',
+      'CSS (RWD, SASS, BEM, TailwindCSS, Bootstrap),',
+      'JavaScript (OOP, Design Patterns),',
+      'React (Redux, Hooks, Styled Components, Compound Components, Atomic Design),',
+      'Next (basic knowledge of the concept of server side rendering and how to use it),',
+      'TypeScript (basic knowledge of types)',
     ],
     classes: {
+      list: [
+        classNames.utilities.m.y(10),
+        classNames.utilities.position.relative,
+      ],
       listItem: [
-        classNames.utilities.margin('b', 15, classNames.utilities.dash),
+        classNames.utilities.m.b(15),
+        classNames.utilities.m.l(20),
+        classNames.utilities.text.lh(25),
+        classNames.utilities.text.dash,
+      ],
+      lastListItem: [
+        classNames.utilities.m.l(20),
+        classNames.utilities.text.lh(25),
+        classNames.utilities.text.dash,
       ],
     },
   },
   {
     type: common.header,
     element: elements.h(5),
-    content: '2. Level enough to write basic backend',
-    classes: [classNames.utilities.margin('t', 30), ,],
+    content: '2. Backend:',
+    classes: [classNames.utilities.m.t(40), classNames.utilities.text.lh(30)],
   },
   {
     type: common.list,
@@ -49,31 +62,26 @@ export default [
       listItem: elements.li,
     },
     content: [
-      '- Node/Express (simple E-COMMERCE backend with REST API),',
-      '- MongoDB (simple handle with Express)',
+      'Node/Express (e-commerce REST API),',
+      'MongoDB (basic handling with Express),',
+      'PHP (basic syntax and logic)',
     ],
     classes: {
-      listItem: [
-        classNames.utilities.margin('b', 15, classNames.utilities.dash),
+      list: [
+        classNames.utilities.m.t(10),
+        classNames.utilities.m.b(0),
+        classNames.utilities.position.relative,
       ],
-    },
-  },
-  {
-    type: common.header,
-    element: elements.h(5),
-    content: '3. When i have a free time',
-    classes: [classNames.utilities.margin('t', 30), ,],
-  },
-  {
-    type: common.list,
-    elements: {
-      list: elements.ul,
-      listItem: elements.li,
-    },
-    content: ['- PHP (basic level)'],
-    classes: {
       listItem: [
-        classNames.utilities.margin('b', 0, classNames.utilities.dash),
+        classNames.utilities.m.b(15),
+        classNames.utilities.m.l(20),
+        classNames.utilities.text.lh(25),
+        classNames.utilities.text.dash,
+      ],
+      lastListItem: [
+        classNames.utilities.m.l(20),
+        classNames.utilities.text.lh(25),
+        classNames.utilities.text.dash,
       ],
     },
   },
