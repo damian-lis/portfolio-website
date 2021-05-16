98dc511932de4bf984ac203ce392fe82357a55c9
Damian Lis <foxprogramming93@gmail.com>
2021-05-16 20:34:47 +0200

    Modified sneakPeeks.js by adding and removing various content

--- data/descriptions/projects/sneakPeeks.js
+++ data/descriptions/projects/sneakPeeks.js
@@ -1,37 +1,68 @@
 export default [
   {
+    route: '/',
+    image: '/images/projects/PortfolioWebsite/introDesktop.gif',
+    alt: 'Potfolio Website',
+    title: 'Portfolio Website (OOP)',
+    intro:
+      'The page You are on that contains information about me, my skills and the projects that I have done. Also You can here send an e-mail to me (support by Emails Handler app)',
+    icons: [
+      { image: '/images/icons/technologies/html.jpg' },
+      { image: '/images/icons/technologies/css.jpg' },
+      { image: '/images/icons/technologies/js.jpg' },
+    ],
+  },
+
+  {
+    route: '/',
+    image: '/images/projects/TalkToGisapiaAndTheOthers/introDesktop.gif',
+    alt: 'Talk to Gisapia and the Others',
+    title: 'Talk to Gisapia and the Others (OOP)',
+    intro:
+      'This bilingual chat bot allows You to chat with various amazing characters who remember, understand and send information about You to Your email! (support by Emails Handler app)',
+    icons: [
+      { image: '/images/icons/technologies/html.jpg' },
+      { image: '/images/icons/technologies/css.jpg' },
+      { image: '/images/icons/technologies/js.jpg' },
+      { image: '/images/icons/technologies/gsap.svg' },
+    ],
+  },
+
+  {
+    route: '/',
+    image: '/images/projects/EmailsHandler/emailsHandlerIntro.gif',
+    alt: 'Emails Handler',
+    title: 'Emails Handler',
+    intro:
+      'A backend application that allows Talk to Gisapia and the Others app and the Porftolio Website to send emails.',
+    icons: [
+      { image: '/images/icons/technologies/nodejs.jpg' },
+      { image: '/images/icons/technologies/sendgrid.jpg' },
+      { image: '/images/icons/technologies/mailgun.jpg' },
+      { image: '/images/icons/technologies/nodemailer.svg' },
+    ],
+  },
+
+  {
     route: '/fluentBlog.html',
     image: '/images/projects/FluentBlog/browseArticle.gif',
     alt: 'FluentBlog',
     title: 'FluentBlog',
-    intro: 'A small blog that uses server side rendering in next.js',
+    intro:
+      'A blog with various articles about the IT industry that uses server side rendering in Next.js technology.',
     icons: [
       { image: '/images/icons/technologies/tailwindcss.jpg' },
       { image: '/images/icons/technologies/next.jpg' },
     ],
   },
-  {
-    route: '/',
-    image: '',
-    alt: 'Talk to Gisapia and the Others',
-    title: 'Talk to Gisapia and the Others',
-    intro:
-      'This little game will let you chat with amazing characters who will remember and understand your answers!',
-    icons: [
-      { image: '/images/icons/technologies/html.jpg' },
-      { image: '/images/icons/technologies/css.jpg' },
-      { image: '/images/icons/technologies/js.jpg' },
-      { image: '/images/icons/technologies/nodejs.jpg' },
-      { image: '/images/icons/technologies/sendgrid.jpg' },
-    ],
-  },
+
   {
     route: '/',
     image: '/images/icons/projects/uniqueChoice.jpg',
     alt: 'Unique Choice',
     title: 'Unique Choice',
     intro:
-      'It is an E-Commerce fullstack project with all the functionality that allows you to make purchases.',
+      'It is an E-Commerce fullstack project with all the functionality that allows You to make purchases.',
     icons: [
       { image: '/images/icons/technologies/bootstrap.jpg' },
       { image: '/images/icons/technologies/react.jpg' },
@@ -41,26 +72,13 @@ export default [
     duringDevelopment: true,
   },
 
-  // {
-  //   route: '/',
-  //   image: '/images/projects/FastHost/icon.jpg',
-  //   alt: 'FastHost',
-  //   title: 'FastHost',
-  //   intro: 'A project with a modern layout and a small own library of styles',
-  //   icons: [
-  //     { image: '/images/icons/technologies/html.jpg' },
-  //     { image: '/images/icons/technologies/css.jpg' },
-  //   ],
-  //   duringDevelopment: true,
-  // },
-
   {
     route: '/',
     image: '/images/icons/projects/leaf.jpg',
     alt: 'Leaf',
     title: 'Leaf',
     intro:
-      'It is a fullstack project that aims to recreate many functionalities, structures and styles from Apple.com',
+      'It is a fullstack project that aims to recreate many functionalities, structures and styles from apple.com.',
     icons: [
       { image: '/images/icons/technologies/bootstrap.jpg' },
       { image: '/images/icons/technologies/react.jpg' },
