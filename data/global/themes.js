724f7a859a4c454e4f4c0cca45c1687cd775b23f
Damian Lis <foxprogramming93@gmail.com>
2021-05-18 17:38:50 +0200

    Increased transparency of main and secondary colors in themes.js

--- data/global/themes.js
+++ data/global/themes.js
@@ -1,10 +1,10 @@
 export default {
   black: {
     mainColor: 'rgb(18, 18, 18)',
-    mainColorTransparent: 'rgba(18, 18, 18, 0.9)',
+    mainColorTransparent: 'rgba(18, 18, 18, 0.8)',
 
     secondaryColor: 'rgb(0,0,0)',
-    secondaryColorTransparent: 'rgba(0, 0, 0, 0.9)',
+    secondaryColorTransparent: 'rgba(0, 0, 0, 0.8)',
 
     borderColor: 'rgb(85 ,85, 85)',
 
@@ -24,10 +24,10 @@ export default {
 
   blue: {
     mainColor: 'rgb(21, 32, 43)',
-    mainColorTransparent: 'rgba(21, 32, 43, 0.9)',
+    mainColorTransparent: 'rgba(21, 32, 43, 0.8)',
 
     secondaryColor: 'rgb(25, 39, 52)',
-    secondaryColorTransparent: 'rgba(25, 39, 52, 0.9)',
+    secondaryColorTransparent: 'rgba(25, 39, 52, 0.8)',
 
     borderColor: 'rgb(57, 119, 179)',
 
@@ -47,10 +47,10 @@ export default {
 
   purple: {
     mainColor: 'rgb(70, 52, 78)',
-    mainColorTransparent: 'rgba(70, 52, 78, 0.9)',
+    mainColorTransparent: 'rgba(70, 52, 78, 0.8)',
 
     secondaryColor: 'rgb(56, 42, 63)',
-    secondaryColorTransparent: 'rgba(56, 42, 63, 0.9)',
+    secondaryColorTransparent: 'rgba(56, 42, 63, 0.8)',
 
     borderColor: 'rgb(154, 0, 154)',
 
