ba5302f5b4d6baad28c20502f2560bed47cd0e89
Damian Lis <foxprogramming93@gmail.com>
2021-05-18 17:21:53 +0200

    Added black theme and changed hex on rgb/rgba in themes.js

--- data/global/themes.js
+++ data/global/themes.js
@@ -1,59 +1,70 @@
 export default {
+  black: {
+    mainColor: 'rgb(18, 18, 18)',
+    mainColorTransparent: 'rgba(18, 18, 18, 0.9)',
+
+    secondaryColor: 'rgb(0,0,0)',
+    secondaryColorTransparent: 'rgba(0,0,0,0.9)',
+
+    borderColor: 'rgb(85 ,85, 85)',
+
+    strokeColor: '85 ,85, 85',
+    ctxFillStyle: 'rgb(85 ,85, 85)',
+
+    mainText: 'rgb(255,255,255)',
+    secondaryText: 'rgb(238,238,238)',
+
+    themeDotBorder: 'rgb(255,255,255)',
+
+    previewBg: 'rgb(34,34,34)',
+    previewShadow: 'rgb(18,18,18)',
+
+    buttonColor: 'rgb(51,51,51)',
+  },
+
   blue: {
-    mainColor: '#15202b',
-    secondaryColor: '#192734',
+    mainColor: 'rgb(21, 32, 43)',
+    mainColorTransparent: 'rgba(21, 32, 43, 0.9)',
+
+    secondaryColor: 'rgb(25, 39, 52)',
+    secondaryColorTransparent: 'rgba(25, 39, 52, 0.9)',
 
     borderColor: 'rgb(57, 119, 179)',
 
     strokeColor: '57, 119, 179',
     ctxFillStyle: 'rgb(57, 119, 179)',
 
-    mainText: '#fff',
-    secondaryText: '#eeeeee',
+    mainText: 'rgb(255,255,255)',
+    secondaryText: 'rgb(238,238,238)',
 
-    themeDotBorder: '#fff',
+    themeDotBorder: 'rgb(255,255,255)',
 
     previewBg: 'rgb(25, 39, 52, 0.8)',
-    previewShadow: '#111921',
+    previewShadow: 'rgb(17,25,33)',
 
-    buttonColor: '#214569',
+    buttonColor: 'rgb(33,69,105)',
   },
-  green: {
-    mainColor: '#606b56',
-    secondaryColor: '#515a48',
 
-    borderColor: 'rgb(169, 209, 132)',
-
-    strokeColor: '169, 209, 132',
-    ctxFillStyle: 'rgb(169, 209, 132)',
-
-    mainText: '#fff',
-    secondaryText: '#eeeeee',
-
-    themeDotBorder: '#fff',
-
-    previewBg: 'rgb(81, 90, 72, 0.8)',
-    previewShadow: '#2a2f25',
-
-    buttonColor: '#6b8059',
-  },
   purple: {
-    mainColor: '#46344e',
-    secondaryColor: '#382a3f',
+    mainColor: 'rgb(70, 52, 78)',
+    mainColorTransparent: 'rgba(70, 52, 78, 0.9)',
+
+    secondaryColor: 'rgb(56, 42, 63)',
+    secondaryColorTransparent: 'rgba(56, 42, 63, 0.9)',
 
     borderColor: 'rgb(154, 0, 154)',
 
     strokeColor: '154, 0, 154',
     ctxFillStyle: 'rgb(154, 0, 154)',
 
-    mainText: '#fff',
-    secondaryText: '#eeeeee',
+    mainText: 'rgb(255,255,255)',
+    secondaryText: 'rgb(238,238,238)',
 
-    themeDotBorder: '#fff',
+    themeDotBorder: 'rgb(255,255,255)',
 
     previewBg: 'rgb(29, 21, 32, 0.8)',
-    previewShadow: '#2b202f',
+    previewShadow: 'rgb(43,32,47)',
 
-    buttonColor: '#5c3669',
+    buttonColor: 'rgb(92,54,105)',
   },
 }
