4f2273691081ffb3dbfa64d5942007e2bf07b3f3
Damian Lis <foxprogramming93@gmail.com>
2021-06-05 13:34:53 +0200

    Modified a local export for the data/descriptions folder

--- data/descriptions/index.js
+++ data/descriptions/index.js
@@ -1,11 +1,18 @@
-import about from './about.js'
-import privacyPolicy from './privacyPolicy.js'
-import skills from './skills.js'
-import { fluentBlog } from './projects/index.js'
+import { about, skills } from './sections/index.js'
+import {
+  fluentBlog,
+  emailsHandler,
+  privacyPolicy,
+  portfolioWebsite,
+  talkToGisapiaAndTheOthers,
+} from './sites/index.js'
 
 export {
   about as aboutDescription,
   privacyPolicy as privacyPolicyDescription,
   skills as skillsDescription,
   fluentBlog as fluentBlogDescription,
+  emailsHandler as emailsHandlerDescription,
+  portfolioWebsite as portfolioWebsiteDescription,
+  talkToGisapiaAndTheOthers as talkToGisapiaAndTheOthersDescription,
 }
