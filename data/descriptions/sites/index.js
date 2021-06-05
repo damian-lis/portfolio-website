6d3b7995c82fd996cd2bee19bb0ed352062b4f74
Damian Lis <foxprogramming93@gmail.com>
2021-06-05 13:34:20 +0200

    Modified a local export for the data/descriptions/sites folder

---
+++ data/descriptions/sites/index.js
@@ -0,0 +1,13 @@
+import fluentBlog from './fluentBlog.js'
+import emailsHandler from './emailsHandler.js'
+import privacyPolicy from './privacyPolicy.js'
+import portfolioWebsite from './portfolioWebsite.js'
+import talkToGisapiaAndTheOthers from './talkToGisapiaAndTheOthers.js'
+
+export {
+  fluentBlog,
+  emailsHandler,
+  privacyPolicy,
+  portfolioWebsite,
+  talkToGisapiaAndTheOthers,
+}
