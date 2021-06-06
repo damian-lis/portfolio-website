116c4a03e4555bdcc050f030fb54ceb75026a602
Damian Lis <foxprogramming93@gmail.com>
2021-06-06 16:31:28 +0200

    Modified project description in portfolioWebsite.js file

--- data/descriptions/sites/portfolioWebsite.js
+++ data/descriptions/sites/portfolioWebsite.js
@@ -1,17 +1,6 @@
-import { p, h, a } from '/data/descriptions/structure.js'
+import { p, a } from '/data/descriptions/structure.js'
 
 export default [
-  h(
-    `Announcement
-    `,
-    {
-      align: 'center',
-      size: 2,
-      mt: 10,
-      smAlign: 'center',
-    }
-  ),
-
   p([
     `The full description is available
     ${a(
