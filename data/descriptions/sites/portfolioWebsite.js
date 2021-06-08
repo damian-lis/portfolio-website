baa1c26bb04d430ed8f603ed033e75e123164624
Damian Lis <foxprogramming93@gmail.com>
2021-06-08 10:21:52 +0200

    Modified data/descriptions/sites/portfolioWebsite.js file

--- data/descriptions/sites/portfolioWebsite.js
+++ data/descriptions/sites/portfolioWebsite.js
@@ -1,12 +1,18 @@
-import { p, a } from '/data/descriptions/structure.js'
+import { p, a, b, im } from '/data/descriptions/structure.js'
 
 export default [
   p([
-    `The full description is available
+    `The full description is available on my github
     ${a(
       'here',
       'https://github.com/damian-lis/portfolio-website/blob/main/README.md'
     )}
-    and has not been included on this page due to the very large size.`,
+    and has not been included on this page due to the very large size (very detailed description).`,
   ]),
+
+  b(),
+
+  p([`This is what the description looks like on my github:`]),
+
+  im(`portfolioWebsite/portfolioWebsiteGitHub.gif`, { mb: 0 }),
 ]
