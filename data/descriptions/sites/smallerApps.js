71e5ae6dfdc781bda217ceba008e428e6920a2cb
Damian Lis <foxprogramming93@gmail.com>
2021-07-10 14:33:19 +0200

    Changed titles, names of photos and links regarding the description of smaller apps

--- data/descriptions/sites/smallerApps.js
+++ data/descriptions/sites/smallerApps.js
@@ -3,84 +3,84 @@ import { p, a, h, im } from '/data/descriptions/structure.js';
 const folder = (path) => `SmallerApps/${path}.gif`;
 
 export default [
-  h(`1. Contact book app`, { mt: 0 }),
+  h(`1. My Contact Book App`, { mt: 0 }),
 
   p(['Technologies: React/Redux/Material-UI/GoogleLogin/Express/MongoDB']),
 
-  im(folder('contactBook'), { mb: 0 }),
+  im(folder('myContactBookApp'), { mb: 0 }),
 
   p([
     'An application that allows you to log into the system in which you can add or remove various contacts.',
   ]),
 
   p([
-    `Live version is ${a('here', 'https://contact-book-frontend.vercel.app/')}.`,
+    `Live version is ${a('here', 'https://my-contact-book-frontend-app.vercel.app/')}.`,
 
     `Github (frontend) is ${a(
       'here',
-      'https://github.com/damian-lis/contact-book-frontend'
+      'https://github.com/damian-lis/my-contact-book-frontend-app'
     )}.`,
 
     `Github (backend) is ${a(
       'here',
-      'https://github.com/damian-lis/contact-book-backend'
+      'https://github.com/damian-lis/my-contact-book-backend-app'
     )}.`,
   ]),
 
-  h(`2. Apple navbar and footer`),
+  h(`2. My Apple App`),
 
   p(['Technologies: React(Styled Components/Compound Components)']),
 
-  im(folder('apple'), { mb: 0 }),
+  im(folder('myAppleApp'), { mb: 0 }),
 
   p(['A website that maps navbar and footer from apple.com']),
 
   p([
-    `Live version is ${a('here', 'https://apple-navbar-footer.vercel.app/')}.`,
+    `Live version is ${a('here', 'https://my-apple-app.vercel.app/')}.`,
 
     `Github is ${a(
       'here',
-      'https://github.com/damian-lis/apple-navbar-footer'
+      'https://github.com/damian-lis/my-apple-app'
     )}.`,
   ]),
 
-  h(`3. Task list app`),
+  h(`3. My Task List TS App`),
 
   p(['Technologies: React/Redux/TypeScript/Material-UI']),
 
-  im(folder('task'), { mb: 0 }),
+  im(folder('myTaskListTsApp'), { mb: 0 }),
 
   p([
     'Application thanks to which you can create various lists of tasks, and in them specific tasks to be performed.',
   ]),
 
   p([
-    `Live version is ${a('here', 'https://task-list-app-ts.herokuapp.com/')}.`,
+    `Live version is ${a('here', 'https://my-task-list-ts-app.herokuapp.com/')}.`,
 
     `Github is ${a(
       'here',
-      'https://github.com/damian-lis/task-list-app'
+      'https://github.com/damian-lis/my-task-list-ts-app'
     )}.`,
   ]),
 
-  h(`4. Extended form`),
+  h(`4. My Formik App`),
 
   p(['Technologies: React/Formik/Yup/Material-UI']),
 
-  im(folder('formik'), { mb: 0 }),
+  im(folder('myFormikApp'), { mb: 0 }),
 
   p(['An extensive form with various fields to fill in.']),
 
   p([
-    `Live version is ${a('here', 'https://extended-form-with-formik-yup-and-material-ui.vercel.app/')}.`,
+    `Live version is ${a('here', 'https://my-formik-app.vercel.app/')}.`,
 
     `Github is ${a(
       'here',
-      'https://github.com/damian-lis/extended-form-with-Formik-Yup-and-Material-UI'
+      'https://github.com/damian-lis/my-formik-app'
     )}.`,
   ]),
 
-  h(`5. Countries app`),
+  h(`5. My Countries App`),
 
   p(['Technologies: React/GraphQL']),
 
@@ -89,11 +89,11 @@ export default [
   p(['An application thanks to which we can search for a specific country.']),
 
   p([
-    `Live version is ${a('here', 'https://graphql-countries-exercise.vercel.app/')}.`,
+    `Live version is ${a('here', 'https://my-countries-app.vercel.app/')}.`,
 
     `Github is ${a(
       'here',
-      'https://github.com/damian-lis/graphql-countries-exercise'
+      'https://github.com/damian-lis/my-countries-app'
     )}.`,
   ]),
 ];
